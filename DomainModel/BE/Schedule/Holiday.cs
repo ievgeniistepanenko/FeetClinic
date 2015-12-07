23acc8c4da46a7c6533d3b84976e37615e7c0745
jk_km@ukr.net <jk_km@ukr.net>
2015-12-07 19:36:41 +0100

    Holiday back

--- DomainModel/BE/Schedule/Holiday.cs
+++ DomainModel/BE/Schedule/Holiday.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DomainModel.BE.Schedule
+namespace BE.BE.Schedule
 {
     public class Holiday
     {
-        private int DayOfYear;
-        private int yearsSchedulerId;
+        public int Id { get; set; }
+        private DateTime _startDate;
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set { _startDate = value.Date; }
+        }
+        private DateTime _endDate;
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set { _endDate = value.Date; }
+        }
 
+        public Holiday(DateTime startDate, DateTime endDate)
+        {
+            _startDate = startDate;
+            _endDate = endDate;
+        }
     }
 }
+
