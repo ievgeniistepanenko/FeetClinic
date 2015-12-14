a7c6762abb30d63ef6d948c52f6098dc5c8c0fbc
andybechmann <andybechmann@hotmail.com>
2015-12-14 10:30:47 +0100

    TimeslotServiceGateway

--- FeetClinic.WEB/ServiceGateway/TimeSlotServiceGateway.cs
+++ FeetClinic.WEB/ServiceGateway/TimeSlotServiceGateway.cs
@@ -6,7 +6,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Configuration;
-using BE.BE;
 using BE.Interfaces;
 
 namespace FeetClinic.WEB.ServiceGateway
@@ -27,7 +26,7 @@ namespace FeetClinic.WEB.ServiceGateway
             IEnumerable<IEnumerable<ITimeSlot>> timeSlots = new List<List<ITimeSlot>>();
             HttpClient client = GetHttpClient();
             HttpResponseMessage response = client
-                .GetAsync(path + "?therapistId=" + therapistId + "&week="+week+"&year="+year).Result;
+                .GetAsync(path + "?therapistId=" + therapistId + "&week=" + week + "&year=" + year).Result;
 
             if (response.StatusCode == HttpStatusCode.OK)
             {
@@ -42,7 +41,7 @@ namespace FeetClinic.WEB.ServiceGateway
                 throw new Exception(response.ReasonPhrase);
             }
             return timeSlots;
-        } 
+        }
 
 
         private HttpClient GetHttpClient()
