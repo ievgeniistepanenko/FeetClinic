e186f5b1eea0b9d4a4cadc11396851d22311466b
jk_km@ukr.net <jk_km@ukr.net>
2015-12-21 13:10:48 +0100

    Log in bag fixed Treatment og Therapist Controller refactored (validation added)

--- FeetClinic.WEB/Models/TreatmentViewModels.cs
+++ FeetClinic.WEB/Models/TreatmentViewModels.cs
@@ -10,22 +10,24 @@ namespace FeetClinic.WEB.Models
     public class TreatmentCreateViewModel
     {
         [Display(Name = "Treatment Type")]
+        [Required(ErrorMessage = "Choose type for treatment")]
         public int SelectedTypeId { get; set; }
         public IEnumerable<SelectListItem> Types { get; set; }
 
         [Display(Name = "Therapist")]
+        [Required(ErrorMessage = "Choose therapists")]
         public int[] SelectedTherapistId { get; set; }
         public IEnumerable<SelectListItem> Therap { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Enter the name for treatment")]
         public string Name { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Enter description")]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Enter the price for treatment")]
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Enter duration i format tt:mm:ss")]
         public TimeSpan Duration { get; set; }
     }
 
